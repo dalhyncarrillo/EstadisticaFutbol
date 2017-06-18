f85f1f31de2a50b8f73e47cf24069078b5deff5e
Juan Moris <moris_juan@hotmail.com>
2017-06-17 21:18:06 -0400

    Agregar archivos de proyecto.

---
+++ EstadisticaFutbol/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstadisticaFutbol.Startup))]
+namespace EstadisticaFutbol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
